f136df880cccd485498b5b0f2a0d50e4e996e1b2
Julius Koskela <julius.koskela@unikie.com>
2023-05-30 15:57:18 +0300

    Add  for development dependencies
    Signed-off-by: Julius Koskela <julius.koskela@unikie.com>

---
+++ shell.nix
@@ -0,0 +1,14 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {
+	nativeBuildInputs = with pkgs; [
+		gnumake
+		pkg-config
+		openapi-generator-cli
+		openssl.dev
+	];
+	buildInputs = with pkgs; [
+		rustc
+		cargo
+		gcc
+	];
+}
